866c3f8664b7f56475156ccbddc1b930b829c65b
lll <flll@users.noreply.github.com>
2020-11-04 14:42:24 +0900

    Update 1.bash

--- 1.bash
+++ 1.bash
@@ -2,20 +2,21 @@
 set -o pipefail
 cd `dirname $0`
 
-if [ ! -f ~/.env/DATA ]; then
+if [ ! -f ~/.envi/DATA ]; then
+    [[ ! -d ~/.envi ]] && mkdir ~/.envi
     read -p "ドメイン名を入力してください > " DOMAINNAME
     [[ -z "${DOMAINNAME}" ]] && echo "ドメイン名を入力してください。もう一度やり直してください。" && exit 1
-    echo -n "${DOMAINNAME} " > ~/.env/DATA
+    echo -n "${DOMAINNAME} " > ~/.envi/DATA
 
     read -p "メールアドレスを入力してください > " MAILADD
     [[ -z "${MAILADD}" ]] && echo "メールアドレスを入力してください。もう一度やり直してください。" && exit 1
     #https://www.regular-expressions.info/email.html
     regex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
     [[ ! ${MAILADD} =~ ${regex} ]] && echo "メールアドレスの構文が間違っています。" && echo "ドメイン名とメールアドレスが逆になっていないか、もしくはメールアドレスをお確かめください" && exit 1
-    echo -n ${MAILADD} > ~/.env/DATA
+    echo -n ${MAILADD} > ~/.envi/DATA
 fi
 
-export `cat ~/.env/DATA | (read aaaa bbbb; echo "DOMAINNAME=$aaaa MAILADD=$bbbb")`
+export `cat ~/.envi/DATA | (read aaaa bbbb; echo "DOMAINNAME=$aaaa MAILADD=$bbbb")`
 
 if [ ! -f ~/nginx-persistence/cert/server.key ]; then
 docker run \
@@ -38,8 +39,8 @@ fi
 
 exit 0
 
-export ROOTPASSWD=`cat /dev/urandom | tr -dc '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.+\-!' | fold -w 20 | head -n 1`
-export DBPASSWD=`cat /dev/urandom | tr -dc '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.+\-!' | fold -w 20 | head -n 1`
+export ROOTPASSWD=`cat /dev/urandom | tr -dc '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' | fold -w 50 | head -n 1`
+export DBPASSWD=`cat /dev/urandom | tr -dc '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' | fold -w 50 | head -n 1`
 export COMPOSE_PROJECT_NAME=$DOMAINNAME
 
 docker-compose up
