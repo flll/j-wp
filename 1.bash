c65ab6bb4fa3f5aae6645e8a8c30db58f388a2e0
lll <flll@users.noreply.github.com>
2020-11-04 21:37:46 +0900

    Add files via upload

--- 1.bash
+++ 1.bash
@@ -18,21 +18,24 @@ fi
 
 export `cat ~/.envi/DATA | (read aaaa bbbb; echo "DOMAINNAME=$aaaa MAILADD=$bbbb")`
 
-if [ ! -f ~/nginx-persistence/cert/server.key ]; then
+if [ ! -f ~/nginx-persistence/cert/${DOMAINNAME}.key ] || [ ! -f ~/nginx-persistence/cert/${DOMAINNAME}.key ]; then
 docker run \
     --rm \
     -p "80:80" \
-    -v ~/nginx-persistence/cert:/cert \
-    -e LEGO_PATH="/cert" \
+    -v ~/nginx-persistence/src-cert:/src-cert \
+    -v ~/nginx-persistence/lego:/lego \
+    -e LEGO_PATH="/lego" \
     goacme/lego \
-        --email "$MAILADD" \
-        --domains "$DOMAINNAME" \
+        --email "${MAILADD}" \
+        --domains "${DOMAINNAME}" \
         --accept-tos \
         --key-type ec384 \
+        --
         --http \
         --filename "server" \
         run \
             --must-staple
+
 
 fi
 
@@ -40,6 +43,6 @@ exit 0
 
 export ROOTPASSWD=`cat /dev/urandom | tr -dc '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' | fold -w 50 | head -n 1`
 export DBPASSWD=`cat /dev/urandom | tr -dc '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' | fold -w 50 | head -n 1`
-export COMPOSE_PROJECT_NAME=$DOMAINNAME
+export COMPOSE_PROJECT_NAME=${DOMAINNAME}
 
 docker-compose up
