919a2809eda63ab1c7dcae91cf75539a4350a70e
flll <no.5@outlook.jp>
2020-11-07 19:00:47 +0000

    とりあえずコミット

--- 1.bash
+++ 1.bash
@@ -5,7 +5,7 @@ cd `dirname $0`
 if [ ! -f ~/.envi/DATA ]; then
     [[ ! -d ~/.envi ]] && mkdir ~/.envi
     echo "ドメイン名を入力してください。例)example.com"
-    echo -e -n "サブドメインを設定している場合、含めてください。例)◯◯◯◯.example.com\n >"
+    echo -e -n "サブドメインを設定している場合、含めてください。例)○○○○.example.com\n > "
     read DOMAINNAME
     [[ -z "${DOMAINNAME}" ]] && echo "ドメイン名を入力してください。もう一度やり直してください。" && exit 1
 
@@ -14,7 +14,7 @@ if [ ! -f ~/.envi/DATA ]; then
     #https://www.regular-expressions.info/email.html
     regex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
     MAIL_SYNTAXERR_MESSAGE="メールアドレスの構文が間違っています。\nドメイン名とメールアドレスが逆になっていないか、もしくはメールアドレスをお確かめください"
-    [[ ! ${MAILADD} =~ ${regex} ]] && echo -e ${MAIL_SYNTAXERR_MESSAGE} && exit 1
+    [[ ! ${MAILADD} =~ $regex ]] && echo -e ${MAIL_SYNTAXERR_MESSAGE} && exit 1
     echo -n "${DOMAINNAME} " > ~/.envi/DATA
     echo -n "${MAILADD}" >> ~/.envi/DATA
     echo "thank you"
@@ -22,10 +22,7 @@ fi
 
 export `cat ~/.envi/DATA | (read aaaa bbbb; echo "DOMAINNAME=$aaaa MAILADD=$bbbb")`
 
-# if [ ! -f ~/lego-persistence/certificates/${DOMAINNAME}.key ] || [ ! -f ~/lego-persistence/certificates/${DOMAINNAME}.key ]; then
-
-echo atart
-echo run lego
+if [ ! -f ~/lego-persistence/certificates/${DOMAINNAME}.key ]; then
 docker run \
     --rm \
     -v ~/lego-persistence:/lego \
@@ -36,19 +33,19 @@ docker run \
         --domains "${DOMAINNAME}" \
         --accept-tos \
         --key-type ec384 \
-        --server=https://acme-staging-v02.api.letsencrypt.org/directory \
         --tls \
-            run \
-            --must-staple
-echo end
-sudo chown `echo $USER` -R ~/lego-persistence
+            run
 
-# fi
+sudo chown `echo $USER` -R ~/lego-persistence
+fi
+
+#シンボリック追加
 
 exit 0
 
-export ROOTPASSWD=`cat /dev/urandom | tr -dc '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' | fold -w 50 | head -n 1`
-export DBPASSWD=`cat /dev/urandom | tr -dc '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' | fold -w 50 | head -n 1`
+passleng=1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+export ROOTPASSWD=`cat /dev/urandom | tr -dc '$passleng' | fold -w 80 | head -n 1`
+export DBPASSWD=`cat /dev/urandom | tr -dc '$passleng' | fold -w 50 | head -n 1`
 export COMPOSE_PROJECT_NAME=${DOMAINNAME}
 
 docker-compose up
