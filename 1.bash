235d2be99bf8903d1d762ea584bf39dab76db5ee
flll <flll@users.noreply.github.com>
2020-11-11 07:24:48 +0900

    dhparm追加

--- 1.bash
+++ 1.bash
@@ -57,6 +57,8 @@ fi
 
 exit 0
 
+openssl dhparam -out ~/certbot-persistence/letsencrypt/live/${DOMAIN}/dhparam 2048
+
 passleng=1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 export ROOTPASSWD=`cat /dev/urandom | tr -dc '$passleng' | fold -w 80 | head -n 1`
 export DBPASSWD=`cat /dev/urandom | tr -dc '$passleng' | fold -w 50 | head -n 1`
