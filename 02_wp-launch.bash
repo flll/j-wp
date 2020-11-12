274ae82a057ed9065b363284b8f3f3bc70b4f74c
flll <flll@users.noreply.github.com>
2020-11-13 00:41:33 +0900

    pgen function追加

--- 02_wp-launch.bash
+++ 02_wp-launch.bash
@@ -17,9 +17,10 @@ read -p "サイト名> " SITE_NAME
 ## ./.${SITE_NAME}_DATA から読み取り、変数にする
 export `cat ./.${SITE_NAME}_DATA | (read aaaa bbbb cccc; echo "SITE_NAME=${aaaa} DOMAINNAME=${bbbb} MAILADD=${cccc}")`
 
-
-passleng=1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
-export ROOTPASSWD=`cat /dev/urandom | tr -dc '$passleng' | fold -w 80 | head -n 1`
-export DBPASSWD=`cat /dev/urandom | tr -dc '$passleng' | fold -w 50 | head -n 1`
+pgen () {
+    cat /dev/urandom | tr -dc [A-Za-z0-9] | fold -w $1 | head -n 1;
+}
+export ROOTPASSWD=`pgen 100`
+export DBPASSWD=`pgen 100`
 
 docker-compose -f 02_serverside.dockercompose.yml up -p ${SITE_NAME}
