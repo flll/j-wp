f827587cf87fd763fd9b5fa07fcc18b01d40a81d
flll <flll@users.noreply.github.com>
2020-11-16 01:44:50 +0900

    ref修正

--- 01_init-site.bash
+++ 01_init-site.bash
@@ -13,12 +13,12 @@ cd `dirname $0`
 #  ※サイトの作成、編集を行った場合Nginxを再起動してください。
 ##
 next-lf
-REF=1
-while [ "$REF" = "0" ] ;do
+REF=1; while [ "$REF" = "0" ] ;do
     site-type
     next-lf
 done
-while [ "$REF" = "0" ] ;do
+
+REF=1; while [ "$REF" = "0" ] ;do
     site-edit
     next-lf
 done
