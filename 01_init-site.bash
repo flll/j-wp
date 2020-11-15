c2ca4326250380ab8c9b98f54619c58e6e607e1c
flll <flll@users.noreply.github.com>
2020-11-16 04:44:50 +0900

    ~/siteから~/.site

--- 01_init-site.bash
+++ 01_init-site.bash
@@ -56,9 +56,9 @@ sudo chown `echo $USER` -R ~/certbot
 
 ## ～コンフィグtemplate記述～
 #  nginx conf
-[[ ! -d ~/site/conf.d ]] && mkdir -p ~/site/conf.d && chmod 766 ~/site/conf.d
+[[ ! -d ~/.site/conf.d ]] && mkdir -p ~/.site/conf.d && chmod 766 ~/.site/conf.d
 envsubst '${SITE_NAME} ${DOMAINNAME}' \
-        < ./template-server-block.conf > ~/site/conf.d/block_${SITE_NAME}.conf
+        < ./template-server-block.conf > ~/.site/conf.d/block_${SITE_NAME}.conf
 
 exit 0
 
