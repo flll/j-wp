0d6e2bd085c1ae1314d644c62f1e26d4ba1b7097
flll <flll@users.noreply.github.com>
2020-11-19 22:09:07 +0900

    !!! store移動

--- 03_webserver-launch.bash
+++ 03_webserver-launch.bash
@@ -4,16 +4,7 @@ cd `dirname $0`
 
 . init/func.bash
 
-next-lf
-echo "＝＝＝ 03 Webサーバを起動させます ＝＝＝"
-echo "※あらかしめサイト名を作成しておいてください"
-sleep 3
-## site-type
-REF=1; while [ $REF = 1 ] ;do
-    site-type
-done
-
 ## nginxを落とす
 down-nginx
- 
+
 docker-compose --project-name web --file 03_webserver.dockercompose.yml up
