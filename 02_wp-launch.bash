11b573c8d2e7a7c78789272462e0328d4c94b3af
flll <flll@users.noreply.github.com>
2020-11-21 05:15:41 +0900

    user test & debug削除

--- 02_wp-launch.bash
+++ 02_wp-launch.bash
@@ -19,21 +19,14 @@ site-data-export
 [[ ! -d ~/.site/conf.d ]] && mkdir -p ~/.site/conf.d && chmod 770 ~/.site/conf.d
 envsubst '${SITE_NAME} ${DOMAINNAME}' \
         < ./store/02_template-wp-block.conf > ~/.site/conf.d/block_${SITE_NAME}.conf
-echo 0
 [[ ! -d ~/nginx.d ]] && mkdir ~/nginx.d && sudo chown -hR `id -u`:www-data ~/nginx.d
-echo 1
 [[ ! -d ~/.site/sec ]] && mkdir ~/.site/sec && chmod 700 ~/.site/sec
-echo 2
 [[ ! -f ~/.site/sec/db_root_pass.txt ]] && pgen > ~/.site/sec/db_root_pass.txt
-echo 3
 [[ ! -f ~/.site/sec/db_wp_pass.txt ]] && pgen > ~/.site/sec/db_wp_pass.txt
-echo b
 export ROOTPASSWD=`cat ~/.site/sec/db_root_pass.txt`
 export DBPASSWD=`cat ~/.site/sec/db_wp_pass.txt`
-echo a
+
 down-nginx
-echo a
 docker network create web-net || echo ""
 docker network create wp-db-net || echo ""
 docker-compose -p ${SITE_NAME} --file store/02_wp.dockercompose.yml up
-echo end
