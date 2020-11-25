1a8f87b4d79ef25435976f2af9aee733b607db0a
flll <flll@users.noreply.github.com>
2020-11-25 17:37:02 +0900

    delete.bash 作成

--- 09_delete.bash
+++ 09_delete.bash
@@ -1,7 +1,25 @@
-#!/bin/bash -e
-set -o pipefail
+#!/bin/bash
 
-echo 
+. init/func.bash
+
+echo "サイト名を削除します。"
+echo "事前にバックアップを取得してください。"
+echo "サイト名、基づいたlog情報、が削除されます"
+read -p "続ける場合は y を入力してください > " accept
+[[ ! $accept == [Yy] ]] && exit 0;
+
+[[ ! $REF = 2 ]] && REF=1
+while [ $REF = 1 ] ;do
+    site-type
+done
+
+
+rm -f ~/j.d/site/${SITE_NAME}_DATA
+
+rm -rf ~/j.d/nginx.d/${SITE_NAME}
+
+rm -f ~/j.d/site/conf.d/block_${SITE_NAME}.conf
+
 
 サイトの削除
 サイトに基づいてblockを削除する
