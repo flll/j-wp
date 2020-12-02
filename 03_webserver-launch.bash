2c65c9b71978e23008803815433023b2f4c437a8
flll <flll@users.noreply.github.com>
2020-12-02 16:01:33 +0900

    a

--- 03_webserver-launch.bash
+++ 03_webserver-launch.bash
@@ -7,9 +7,9 @@ cd `dirname $0`
 openssl rand 48 > ~/j.d/lego/tls_session_ticket.key
 
 ## nginxを落とす
-down-nginx
+[[ `docker ps -f name=/web-nginx$ -q` ]] && docker-compose -p web -f ./store/03_webserver.dockercompose.yml down --remove-orphans
 
 docker network create web-net || :
 
 docker-compose -p web --file store/03_webserver.dockercompose.yml up \
-    || echo -e "nginxが起動できませんでした。\n02が成功しているか確認してください。"
+    || echo -e "nginxが起動できませんでした。\n02が成功しているか確認してください。" && docker ps
