bb34b83cabb4e72837fd078f49be6a47d4509cb1
flll <flll@users.noreply.github.com>
2020-12-02 12:33:01 +0900

    restart項目、func.bashに移動

--- init/func.bash
+++ init/func.bash
@@ -120,6 +120,17 @@ function site-edit () {
         REF=0
 }
 
-function down-nginx () {
-    docker-compose -p web -f ./store/03_webserver.dockercompose.yml down --remove-orphans || :
+function restart-nginx () {
+    #  プロジェクト名: web nginxをダウンさせます。
+    #  nginxが起動されていた場合sleepを使って再度起動し直します
+    [[ `docker ps -f name=/web-nginx$ -q` ]] \
+        && echo "!!! nginxが起動しています。いちど、nginxを再起動します" \
+        && echo "!!! サイト名が削除されたのち、nginxが起動します" \
+        && ( # 並列処理 sleepしたあとnginxを起動する
+            sleep 7; docker-compose -p web -f ./store/03_webserver.dockercompose.yml up -d \
+            || echo -e "nginxが起動できませんでした。\n02が成功しているか確認してください。";
+            echo nginxが起動しました
+        )&
+    sleep 2
 }
+
